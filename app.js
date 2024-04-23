8ac6191930f69ab15594e283b5fa2e0aec2dfd35
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-24 00:38:47 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 // write code here
+const array = [
+  { name: 'Ben', age: 14 },
+  { name: 'Phil', age: 18 },
+  { name: 'John', age: 32 },
+  { name: 'Ann', age: 16 },
+  { name: 'Paul', age: 24 }
+];
+
+const result = array
+  .filter(person => person.age >= 18)
+  .map(person => person.name);
+
+console.log(result);
